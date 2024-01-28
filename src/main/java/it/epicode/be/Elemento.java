184eeca349ddee7bba3424a50de00b26eb73b1c4
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-01-28 11:47:46 +0100

    Elemento

--- src/main/java/it/epicode/be/Elemento.java
+++ src/main/java/it/epicode/be/Elemento.java
@@ -14,4 +14,43 @@ public class Elemento {
     public Elemento() {
     }
 
+    public Elemento(String isbn, String titolo, int annoPubblicazione, int numPagine) {
+        this.isbn = isbn;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numPagine = numPagine;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public int getNumPagine() {
+        return numPagine;
+    }
+
+    public void setNumPagine(int numPagine) {
+        this.numPagine = numPagine;
+    }
+
 }
